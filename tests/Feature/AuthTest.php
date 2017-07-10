a6d9e1cb675869f400a9ce8a731ff5695e10443c
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-07-10 22:43:18 +0200

    Auth test

--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -7,7 +7,20 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class AuthTest extends TestCase
 {
+    use DatabaseMigrations;
 
-    
+    /** @test */
+    public function an_authorized_but_non_subscribed_user_will_see_the_plans_page()
+    {
+        // Given we have a non subscribed user
+        $user = create('App\User');
+
+        // When they login
+        $this->signIn($user);
+
+        // They should see the plans page
+        $this->assertRedirect('/plans');
+
+    }
 
 }

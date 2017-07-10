5849505ca32a9aa405acc22b72966b180f8edacd
Asbjørn <asbjorn.lindholm@hotmail.com>
2017-07-10 21:48:56 +0200

    Updated home view and dashboard view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel">
+                    <div class="panel-body">
+                        <h1>Welcome</h1>
 
-                <div class="panel-body">
-                    You are logged in!
+                        <p>This is a business analytics tools</p>
+
+                        <a href="/register" class="btn btn-primary">Get started</a>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
